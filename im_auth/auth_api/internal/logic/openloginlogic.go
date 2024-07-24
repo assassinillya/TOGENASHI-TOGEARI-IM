23e5fc8d8b9448d1d414b42387f15f495379a0c0
asily <1664962271@qq.com>
2024-07-24 14:36:29 +0800

    第三方登录的优化

--- im_auth/auth_api/internal/logic/openloginlogic.go
+++ im_auth/auth_api/internal/logic/openloginlogic.go
@@ -3,6 +3,7 @@ package logic
 import (
 	"context"
 	"errors"
+	"fmt"
 	"github.com/zeromicro/go-zero/core/logx"
 	"im_server/im_auth/auth_api/internal/svc"
 	"im_server/im_auth/auth_api/internal/types"
@@ -10,7 +11,6 @@ import (
 	"im_server/im_user/user_rpc/types/user_rpc"
 	"im_server/utils/jwts"
 	"im_server/utils/open_login"
-	"log"
 )
 
 type Open_loginLogic struct {
@@ -29,52 +29,68 @@ func NewOpen_loginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Open_l
 
 func (l *Open_loginLogic) Open_login(req *types.OpenLoginRequest) (resp *types.LoginResponse, err error) {
 
+	type OpenInfo struct {
+		Nickname string
+		OpenID   string
+		Avatar   string
+	}
+
+	var info OpenInfo
+
 	switch req.Flag {
 	case "qq":
-		info, err := open_login.NewQQLogin(req.Code, open_login.QQConfig{
+		qqInfo, openError := open_login.NewQQLogin(req.Code, open_login.QQConfig{
 			AppID:    l.svcCtx.Config.QQ.AppID,
 			AppKey:   l.svcCtx.Config.QQ.AppKey,
 			Redirect: l.svcCtx.Config.QQ.Redirect,
 		})
-		if err != nil {
-			logx.Error(err)
-			return nil, errors.New("qq登录失败")
+		info = OpenInfo{
+			OpenID:   qqInfo.OpenID,
+			Nickname: qqInfo.Nickname,
+			Avatar:   qqInfo.Avatar,
 		}
-		log.Println(info)
-		var user auth_models.UserModel
-		err = l.svcCtx.DB.Take(&user, "open_id=?", info.OpenID).Error
-		if err != nil {
-			// todo 注册逻辑
-			log.Println("注册服务")
-			res, err := l.svcCtx.UserRpc.UserCreate(context.Background(), &user_rpc.UserCreateRequest{
-				NickName: info.Nickname,
-				Password: "",
-				Role:     2,
-				Avatar:   info.Avatar,
-				OpenId:   info.OpenID,
-			})
-			if err != nil {
-				logx.Error(err)
-				return nil, errors.New("注册失败")
-			}
-			user.Model.ID = uint(res.UserId)
-			user.Role = 2
-			user.Nickname = info.Nickname
-		}
-		//todo 登录逻辑
-		token, err := jwts.GenToken(jwts.JwtPayLoad{
-			UserID:   user.ID,
-			Nickname: user.Nickname,
-			Role:     user.Role,
-		}, l.svcCtx.Config.Auth.AccessSecret, l.svcCtx.Config.Auth.AccessExpire)
-		if err != nil {
-			logx.Error(err)
-			err = errors.New("服务内部错误")
-			return
-		}
-
-		return &types.LoginResponse{Token: token}, nil
+		err = openError
+	default:
+		err = errors.New("不支持的三方登录")
 	}
+	if err != nil {
+		logx.Error(err)
+		return nil, errors.New("登录失败")
+	}
+	var user auth_models.UserModel
+	err = l.svcCtx.DB.Take(&user, "open_id = ?", info.OpenID).Error
+	if err != nil {
+		// 注册逻辑
+		fmt.Println("注册服务")
+		res, err := l.svcCtx.UserRpc.UserCreate(context.Background(), &user_rpc.UserCreateRequest{
+			NickName:       info.Nickname,
+			Password:       "",
+			Role:           2,
+			Avatar:         info.Avatar,
+			OpenId:         info.OpenID,
+			RegisterSource: "qq",
+		})
+		if err != nil {
+			logx.Error(err)
+			return nil, errors.New("登录失败")
+		}
+		user.Model.ID = uint(res.UserId)
+		user.Role = 2
+		user.Nickname = info.Nickname
+	}
+
+	// 登录逻辑
+	token, err1 := jwts.GenToken(jwts.JwtPayLoad{
+		UserID:   user.ID,
+		Nickname: user.Nickname,
+		Role:     user.Role,
+	}, l.svcCtx.Config.Auth.AccessSecret, l.svcCtx.Config.Auth.AccessExpire)
+	if err1 != nil {
+		logx.Error(err1)
+		err = errors.New("服务内部错误")
+		return nil, err1
+	}
+	return &types.LoginResponse{Token: token}, nil
 
 	return
 }
