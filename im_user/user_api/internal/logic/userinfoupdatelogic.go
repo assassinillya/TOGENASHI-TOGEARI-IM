3ce690cc1df669e09fc67f9d93302d7cb6c60f40
asily <1664962271@qq.com>
2024-07-30 21:39:56 +0800

    用户信息修改(暴力写法, 可使用反射优化)

--- im_user/user_api/internal/logic/userinfoupdatelogic.go
+++ im_user/user_api/internal/logic/userinfoupdatelogic.go
@@ -2,8 +2,11 @@ package logic
 
 import (
 	"context"
+	"errors"
+	"im_server/common/models/ctype"
 	"im_server/im_user/user_api/internal/svc"
 	"im_server/im_user/user_api/internal/types"
+	"im_server/im_user/user_models"
 	"im_server/utils/maps"
 	"log"
 
@@ -26,7 +29,71 @@ func NewUserInfoUpdateLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Us
 
 func (l *UserInfoUpdateLogic) UserInfoUpdate(req *types.UserInfoUpdateRequest) (resp *types.UserInfoUpdateResponse, err error) {
 	log.Println(req.UserID)
-	log.Println(maps.RefToMap(*req, "user"))
-	log.Println(maps.RefToMap(*req, "user_conf"))
+	userMaps := maps.RefToMap(*req, "user")
+	if len(userMaps) != 0 {
+		var user user_models.UserModel
+		err = l.svcCtx.DB.Take(&user, req.UserID).Error
+		if err != nil {
+			return nil, errors.New("用户不存在")
+		}
+
+		err = l.svcCtx.DB.Model(&user).Updates(userMaps).Error
+		if err != nil {
+			logx.Error(userMaps)
+			logx.Error(err)
+			return nil, errors.New("用户信息更新失败")
+		}
+	}
+
+	userConfMaps := maps.RefToMap(*req, "user_conf")
+	if len(userConfMaps) != 0 {
+		var userConf user_models.UserConfModel
+		err = l.svcCtx.DB.Take(&userConf, "user_id = ?", req.UserID).Error
+		if err != nil {
+			return nil, errors.New("用户配置不存在")
+		}
+
+		verificationQuestion, ok := userConfMaps["verification_question"]
+		if ok {
+			delete(userConfMaps, "verification_question")
+
+			data := ctype.VerificationQuestion{}
+			if val, ok := verificationQuestion.(map[string]any)["problem1"]; ok {
+				s := val.(string)
+				data.Problem1 = &s
+			}
+			if val, ok := verificationQuestion.(map[string]any)["problem2"]; ok {
+				s := val.(string)
+				data.Problem2 = &s
+			}
+			if val, ok := verificationQuestion.(map[string]any)["problem3"]; ok {
+				s := val.(string)
+				data.Problem3 = &s
+			}
+			if val, ok := verificationQuestion.(map[string]any)["answer1"]; ok {
+				s := val.(string)
+				data.Answer1 = &s
+			}
+			if val, ok := verificationQuestion.(map[string]any)["answer2"]; ok {
+				s := val.(string)
+				data.Answer2 = &s
+			}
+			if val, ok := verificationQuestion.(map[string]any)["answer3"]; ok {
+				s := val.(string)
+				data.Answer3 = &s
+			}
+			l.svcCtx.DB.Model(&userConf).Updates(&user_models.UserConfModel{
+				VerificationQuestion: &data,
+			})
+		}
+
+		err = l.svcCtx.DB.Model(&userConf).Updates(userConfMaps).Error
+		if err != nil {
+			logx.Error(userConfMaps)
+			logx.Error(err)
+			return nil, errors.New("用户配置更新失败")
+		}
+	}
+
 	return
 }
--- im_user/user_api/internal/svc/servicecontext.go
+++ im_user/user_api/internal/svc/servicecontext.go
@@ -2,6 +2,8 @@ package svc
 
 import (
 	"github.com/zeromicro/go-zero/zrpc"
+	"gorm.io/gorm"
+	"im_server/core"
 	"im_server/im_user/user_api/internal/config"
 	"im_server/im_user/user_rpc/types/user_rpc"
 	"im_server/im_user/user_rpc/users"
@@ -9,12 +11,15 @@ import (
 
 type ServiceContext struct {
 	Config  config.Config
+	DB      *gorm.DB
 	UserRpc user_rpc.UsersClient
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	mysqlDb := core.InitGorm(c.Mysql.DataSource)
 	return &ServiceContext{
 		Config:  c,
+		DB:      mysqlDb,
 		UserRpc: users.NewUsers(zrpc.MustNewClient(c.UserRpc)),
 	}
 }
