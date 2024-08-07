c73bf58152fa02c7c76f94cc9ec8d5331bca6982
asily <1664962271@qq.com>
2024-08-07 16:51:48 +0800

    最近会话列表查询完成 - 修改了chat_models中名称不对的bug

--- im_chat/chat_models/chat_model.go
+++ im_chat/chat_models/chat_model.go
@@ -10,7 +10,7 @@ type ChatModel struct {
 	SendUserID uint             `json:"sendUserID"`
 	RevUserID  uint             `json:"revUserID"`
 	MsgType    int8             `json:"msgType"`                                      // 消息类型 1 文本类型 2 图片消息 3 视频消息 4 文件消息 5 语音消息 6 语音通话 7 视频通话 8 撤回消息 9 回复消息 10 引用消息
-	MsgPreView string           `gorm:"column:msg_preview;size:64" json:"msgPreView"` //消息预览
+	MsgPreView string           `gorm:"column:msg_preview;size:64" json:"msgPreview"` //消息预览
 	Msg        ctype.Msg        `json:"msg"`
 	SystemMsg  *ctype.SystemMsg `json:"systemMsg"` //系统提示
 }
