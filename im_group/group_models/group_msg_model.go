97f6db3058fdb12246cae00d9163c9cfba547bc9
asily <1664962271@qq.com>
2024-08-22 17:25:14 +0800

    重构群聊消息处理和群组历史记录逻辑
    - 重构群聊消息类型定义，使用ctype.MsgType替代int8。
    - 增加消息预览方法MsgPreview，封装消息预览逻辑。
    - 重构群聊消息发送逻辑，确保消息正确存储并广播给在线群成员。
    - 调整群聊历史记录逻辑，增加IsMe字段以标识消息发送者。
    -修正消息处理中的拼写错误和时态错误。
    
    此次重构提高了代码的可读性和可维护性，同时修复了潜在的消息发送和存储问题。

--- im_group/group_models/group_msg_model.go
+++ im_group/group_models/group_msg_model.go
@@ -11,8 +11,25 @@ type GroupMsgModel struct {
 	GroupID    uint             `json:"groupID"`                     //群id
 	GroupModel GroupModel       `gorm:"foreignKey:GroupID" json:"-"` //群
 	SendUserID uint             `json:"sendUserID"`                  //发送者id
-	MsgType    int8             `json:"msgType"`                     // 消息类型 1 文本类型 2 图片消息 3 视频消息 4 文件消息 5 语音消息 6 语音通话 7 视频通话 8 撤回消息 9 回复消息 10 引用消息 11 at消息
+	MsgType    ctype.MsgType    `json:"msgType"`                     // 消息类型 1 文本类型 2 图片消息 3 视频消息 4 文件消息 5 语音消息 6 语音通话 7 视频通话 8 撤回消息 9 回复消息 10 引用消息 11 at消息
 	MsgPreView string           `gorm:"size:64" json:"msgPreview"`   //消息预览
 	Msg        ctype.Msg        `json:"msg"`                         //消息内容
 	SystemMsg  *ctype.SystemMsg `json:"systemMsg"`                   //系统提示
+}
+
+func (msg GroupMsgModel) MsgPreviewMethod() string {
+	if msg.SystemMsg != nil {
+		switch msg.SystemMsg.Type {
+		case 1:
+			return "[系统消息]- 该消息涉黄，已被系统拦截"
+		case 2:
+			return "[系统消息]- 该消息涉恐，已被系统拦截"
+		case 3:
+			return "[系统消息]- 该消息涉政，已被系统拦截"
+		case 4:
+			return "[系统消息]- 该消息存在不正当言论，已被系统拦截"
+		}
+		return "[系统消息]"
+	}
+	return msg.Msg.MsgPreview()
 }
